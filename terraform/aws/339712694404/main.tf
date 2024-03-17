7d9651676dc5eddca39b6aebe79bbd079b644fb9
Helio Albano de Oliveira <helioalb@gmail.com>
2024-03-17 11:03:42 -0300

    Add subnets

--- terraform/aws/339712694404/main.tf
+++ terraform/aws/339712694404/main.tf
@@ -2,7 +2,53 @@ resource "aws_vpc" "spree" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name: "Spree"
+    Name = "Spree"
+  }
+
+  provider = aws.virginia
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.spree.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Public 1"
+  }
+
+  provider = aws.virginia
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.spree.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private 1"
+  }
+
+  provider = aws.virginia
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.spree.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Public 2"
+  }
+
+  provider = aws.virginia
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.spree.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private 2"
   }
 
   provider = aws.virginia
